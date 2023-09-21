8f2e97c5964e9ffae46449c3c744f2fbbd831e71
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2023-09-22 01:10:50 +0700

    add trailingSlash: false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rekalogika', // Usually your GitHub org/user name.
   projectName: 'rekalogika-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
